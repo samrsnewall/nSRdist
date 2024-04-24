e72ed839f3c1ccaa239b6272f38dbd7714c56400
samnewall <samnewall@snewall-macmini.geol.ucsb.edu>
2024-04-23 17:07:32 -0700

    added two attempts to calculate the mean of the mixlognorm - need to revise this and finalise

--- Functions/fitMixLogNorm.m
+++ Functions/fitMixLogNorm.m
@@ -4,14 +4,14 @@ function[pdf2save] = fitMixLogNorm(data_linear)
 numcomps = 2;
 %Set the range of x values of interest
 %lx = -3:0.02:10;
-lognorm_BIGMACS = readtable("../lognormal_BIGMACS.txt");
-lx = lognorm_BIGMACS(:,1)';
+lognorm_BIGMACS = readtable("lognormal_BIGMACS.txt");
+lx = lognorm_BIGMACS.Var1';
 %Set the number of iterations (default often doesn't converge)
 options = statset('MaxIter', 1000);
 %Use GMFit to fit a Gaussian Mixture distribution to the data (log of data
 %used)
 data_log = log(data_linear);
-gmfit = fitgmdist(data_log, numcomps, "Options", options);
+gmfit = fitgmdist(data_log', numcomps, "Options", options);
 %Find the means of fit
 mu_fit = gmfit.mu;
 %Initialise std deviation vector and prob density vector
@@ -30,4 +30,13 @@ mix_fit = gmfit.ComponentProportion;
 %multiplying by their mixing component
 lz = sum(ly.*mix_fit, 2);
 pdf2save = [lx', lz];
+% How can I find the mean and standard deviation of a mix log normal
+% distribution
+mean_mixlognormal = mix_fit(1)*exp(mu_fit(1) + (stddev_fit(1).^2)/2) + mix_fit(2) * exp(mu_fit(2) + (stddev_fit(2).^2)/2)
+
+%create samples from each gaussian dist
+numsamps = 2000;
+samps1 = normrnd(mu_fit(1), stddev_fit(1), round(mix_fit(1)*numsamps), 1);
+samps2 = normrnd(mu_fit(2), stddev_fit(2), round(mix_fit(2)*numsamps), 1);
+mean = exp((sum(samps1) + sum(samps2))/(numsamps*2))
 end
