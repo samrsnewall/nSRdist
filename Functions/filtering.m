bf4f96971e5a4bfda16a123a0dba814e6f2d6cda
samnewall <samnewall@snewall-macmini.geol.ucsb.edu>
2024-05-14 15:16:20 -0700

    BIG CHANGE: filtering now also returns the data that was removed (in structures that are related to the reason for their removal) so that this can be plotted/explored. Also, it highlights in plots where very large differences between neighbouring age pairs are.

--- Functions/filtering.m
+++ Functions/filtering.m
@@ -1,33 +1,20 @@
-function[age, depth_cm, error, label, emptybreak1, emptybreak2] = filtering(age, depth_cm, error, label, LabIDs, incDepths, excLabIDs, excDepths)
+function[age, depth, error, label, emptybreak1, emptybreak2, EM, ManE, NotCCR, ageGapTooHigh] = filtering(age, depth, error, label, LabIDs, incDepths, excLabIDs, excDepths)
 
 emptybreak1 = 0;
 emptybreak2 = 0;
 
 %% Choose only MSPF dates
 %Filter the data so it's only those with MSPF
-if strcmp(string(LabIDs), "all") %if string says all, do nothing
-elseif isempty(LabIDs) % if string is empty, check for depths to include
-    if isempty(incDepths) %if no depths and no LabIDs to include, break out of function
-        emptybreak1 = 1; %return signal to break out of function
-        return
-    else
-        %Choose what depths to include
-        incDepthsStr = string(split(incDepths, ', '));
-        incDepthsN = double(incDepthsStr);
-        ind = ismember(depth_cm, incDepthsN.*100);
-
-        %fill outputs
-        age = age(ind);
-        depth_cm = depth_cm(ind);
-        error = error(ind);
-        label = label(ind);
-
-    end
-else %if string has LabIDs, find out which LabIDs they are by comparing with label
-    
+if strcmp(string(LabIDs), "all") %if string says all, no dates are excluded for their material
+    logi1 = true(length(age), 1);
+    EM.age = age(~logi1);
+    EM.depth = depth(~logi1);
+    EM.error = error(~logi1);
+    EM.label = label(~logi1);
+elseif ~isempty(LabIDs) % if string is not empty, check for depths to include
     %Get Good Lab IDs
     LabIDsSplit_Raw = split(LabIDs, ', ');
-    
+
     %Remove problematic whitespace that isn't removed by strip (char 65279
     %causes many problems)
     test4ProblematicWhitespace = contains(LabIDsSplit_Raw, char(65279));
@@ -42,59 +29,115 @@ else %if string has LabIDs, find out which LabIDs they are by comparing with lab
         end
         LabIDsSplitGood = LabIDsSplit_noProblemWhitespace;
     else
-    LabIDsSplitGood = LabIDsSplit_Raw;
+        LabIDsSplitGood = LabIDsSplit_Raw;
     end
-    
+
     %Get indeces of labels that are in LabIDs field
-    ind = contains(strip(string(label)), strip(string(LabIDsSplitGood)));
-   
-    %fill outputs with these indeces
-    age = age(ind);
-    depth_cm = depth_cm(ind);
-    error = error(ind);
-    label = label(ind);
+    logi1 = contains(strip(string(label)), strip(string(LabIDsSplitGood)));
+    if sum(logi1)~=length(LabIDsSplitGood)
+        warning("Some of the LabIDs listed to be chosen in the COPYcorechoices_MSPF file do not match with the LabIDs read in from the WA2022")
+    end
+
+    %fill excluded material (EM) outputs
+    EM.age = age(~logi1);
+    EM.depth = depth(~logi1);
+    EM.error = error(~logi1);
+    EM.label = label(~logi1);
+elseif ~isempty(incDepths) %if there are depths to include include those
+
+    %Choose what depths to include
+    incDepthsStr = string(split(incDepths, ', '));
+    incDepthsN = double(incDepthsStr);
+    logi1 = ismember(depth, incDepthsN.*100);
+
+    %fill excluded material (EM) outputs
+    EM.age = age(~logi1);
+    EM.depth = depth(~logi1);
+    EM.error = error(~logi1);
+    EM.label = label(~logi1);
+
+else %if there is no information to include, signal break out of function
+    emptybreak1 = 1; %return signal to break out of parent function
+    logi1 = false(size(age));
+    EM.age = age;
+    EM.depth = depth;
+    EM.error = error;
+    EM.label = label;
 end
 
-%% Remove manually excluded dates
+%% Remove manually excluded (ManE) dates
 
 %Remove the LabIDs of ages that are clearly erroneous (large age
 %reversal - hand picked)
-if isempty(excLabIDs)
-else
-    ind = contains(string(label), string(split(excLabIDs, ', ')));
-    ind = ind==0;
-    age = age(ind);
-    depth_cm = depth_cm(ind);
-    error = error(ind);
-    label = label(ind);
-end
-
-if isempty(excDepths)
-else
+if ~isempty(excLabIDs)
+    logi3 = contains(string(label), string(split(excLabIDs, ', ')));
+    ManE.age = age(logi3);
+    ManE.depth = depth(logi3);
+    ManE.error = error(logi3);
+    ManE.label = label(logi3);
+elseif ~isempty(excDepths)
     excDepthsStr = string(split(excDepths, ', '));
     excDepthsN = double(excDepthsStr);
-    ind = ismember(depth_cm, excDepthsN.*100);
-    ind = ind==0;
-    age = age(ind);
-    depth_cm = depth_cm(ind);
-    error = error(ind);
-    label = label(ind);
+    logi3 = ismember(depth, excDepthsN.*100);
+    ManE.age = age(logi3);
+    ManE.depth = depth(logi3);
+    ManE.error = error(logi3);
+    ManE.label = label(logi3);
+else
+    logi3 = false(length(age), 1);
+        ManE.age = age(logi3);
+    ManE.depth = depth(logi3);
+    ManE.error = error(logi3);
+    ManE.label = label(logi3);
+    %if there are no dates to manually exclude, move on
 end
 
+%Display warning if both excLabIDs and excDepths have information
+if ~isempty(excLabIDs) & ~isempty(excDepths)
+    warning("Both excLabIDs and excDepths have information. Currently, if excLabIDs has information, excDepths will be ignored.")
+end
 %% Filter ages to fit within calibration curve
 %Filter the data so it only includes those where the age is greater than
 %1000 and below 42,000 yr C14 BP. (to ensure they fit within the limits of
 %the calibration curve)
-ind = find(age-error >= 1 & age+error <=42);
-depth_cm = depth_cm(ind);
-age = age(ind);
-error = error(ind);
-label = label(ind);
+logi4 = age-error >= 1 & age+error <=42;
+NotCCR.depth = depth(~logi4);
+NotCCR.age = age(~logi4);
+NotCCR.error = error(~logi4);
+NotCCR.label = label(~logi4);
+
 
 %% Filter cores to have at least 4 dates remaining (arbitrary choice... chosen to exclude cores with v low data)
 %Filter out cores that have less than 4 dates remaining
 if length(age)<4
-emptybreak2 = 1;
+    emptybreak2 = 1;
 end
+%% Choose cores that fit all criteria
+ChosenLogi = logi1 & ~logi3 & logi4;
 
+%fill outputs
+% ChosenMSPF.age = age(ChosenLogi);
+% ChosenMSPF.depth = depth(ChosenLogi);
+% ChosenMSPF.error = error(ChosenLogi);
+% ChosenMSPF.label = label(ChosenLogi);
+age = age(ChosenLogi);
+depth = depth(ChosenLogi);
+error = error(ChosenLogi);
+label = label(ChosenLogi);
+%% Note where there are age differences greater than 8kyr (using mean of uncalibrated radiocarbon age)
+agediffs = diff(age); %calculate differences between means of uncalibrated radiocarbon age
+logi5 = agediffs > 8; %find locations where differences are greater than 8kyr
+logi6 = false(size(age)); %initiate a logical (default = false) to denote which ages are involved in the differences greater than desired value
+if sum(logi5) ~= 0
+    for i = 1:length(agediffs)
+        if logi5(i) == 1
+            logi6(i) = 1;
+            logi6(i+1) = 1;
+        end
+    end
+end
+ageGapTooHigh.depth = depth(logi6);
+ageGapTooHigh.age = age(logi6);
+ageGapTooHigh.error = error(logi6);
+ageGapTooHigh.label = label(logi6);
 end
