69d7826cff91ba5b06ebba070a72cc17aa4bafce
samnewall <samnewall@snewall-macmini.geol.ucsb.edu>
2024-05-21 15:09:17 -0700

    added some comment

--- Functions/unfind.m
+++ Functions/unfind.m
@@ -1,4 +1,7 @@
 function lgc = unfind(idx, N)
+%This function returns a logical from a set of indices. It is like the
+%reverse of the find function.
+
     %Go from indicies into logical (for vectors only)
     lgc = false(N,1);
     lgc(idx) = true;
