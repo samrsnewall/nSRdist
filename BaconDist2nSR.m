48f21c5c5a1ea1b6ad558a5f1bca276f49446b75
samnewall <samnewall@snewall-macmini.geol.ucsb.edu>
2024-05-21 15:09:59 -0700

    many good changes

--- BaconDist2nSR.m
+++ BaconDist2nSR.m
@@ -1,31 +1,50 @@
-%Use Bacon defaults to get shape and scale factors
-accShape = 1.5;
-accMeanYr = 200; %(yr/cm)
-accMean = accMeanYr/1000; %(kyr/cm)
-accScale = accMean/accShape;
-defaultGamma = makedist("Gamma", "a", accShape, "b", accScale);
+%% README
+%Compare many bacon gamma acc rate distributions to BIGMACS nSR dist
 
-%Find y values for Gamma distribution on normalised SR
-x = 0:(accMean/50):accMean*60;
-yGamma = gampdf(x, accShape, accScale);
+%% Load BIGMACS nSR distribution
+BMLogNorm = load("lognormal_BIGMACS.txt");
 
-%Convert to SR from invSR
-[SR, SRprob] = invSRtoSR(x,yGamma);
+%% Interp values onto same scale as gamma values will be plotted on
+[convnSRExample, convnSRprobExample] = gammaAccRate2nSR(3/2);
+BMLogNormProbInterp = interp1(BMLogNorm(:,1), BMLogNorm(:,2), convnSRExample);
 
-%Convert to normalised SR
-nSR = SR/(1/accMean);
+%% Convert acc rate dists to nSR
+% The value of alpha used is described in halves, i.e. alpha = 1.5 = 3/2,
+% is called alpha3h
 
-% Plot these distributions
-figure()
-subplot(3,1,1)
-plot(x,yGamma)
-ylabel("probability density")
-xlabel("invSR (kyr/cm)")
-subplot(3,1,2)
-plot(SR, SRprob)
-ylabel("Probability Density")
-xlabel("SR (cm/kyr)")
-subplot(3,1,3)
-plot(nSR, SRprob)
-ylabel("Probability Density")
+% [alph1h.nSR, alph1h.nSRprob] = gammaAccRate2nSR(1/2);
+% [alph2h.nSR, alph2h.nSRprob] = gammaAccRate2nSR(2/2);
+% [alph3h.nSR, alph3h.nSRprob] = gammaAccRate2nSR(3/2);
+% [alph4h.nSR, alph4h.nSRprob] = gammaAccRate2nSR(4/2);
+% [alph5h.nSR, alph5h.nSRprob] = gammaAccRate2nSR(5/2);
+% [alph6h.nSR, alph6h.nSRprob] = gammaAccRate2nSR(6/2);
+% [alph7h.nSR, alph7h.nSRprob] = gammaAccRate2nSR(7/2);
+
+figure;
+subplot(2,1,1)
+hold on
+plot(BMLogNorm(:,1), BMLogNorm(:,2), 'k-', 'DisplayName', "BIGMACS MLN", 'LineWidth', 2)
+
+for i = [1,2,3,4,5,6,7]
+    [convnSR, convnSRprob] = gammaAccRate2nSR(i/2);
+    plot(convnSR, convnSRprob, '-', 'DisplayName', "alpha = " + num2str(i)+ "/2", 'LineWidth', 1)
+end
+
 xlabel("nSR")
+ylabel("Probability Density")
+xlim([0 6])
+legend('location', 'northeast')
+
+subplot(2,1,2)
+hold on
+
+for i = [1,2,3,4,5,6,7]
+    [convnSR, convnSRprob] = gammaAccRate2nSR(i/2);
+    plot(convnSR, BMLogNormProbInterp - convnSRprob, '-', 'DisplayName', "alpha = " + num2str(i)+ "/2", 'LineWidth', 1)
+    % NEED TO FIX THE FACT THAT COLOURS ON SUBPLOT 1 and 2 are different!
+end
+
+xlabel("nSR")
+ylabel("Probability Density Anomaly")
+xlim([0 6])
+legend('location', 'northeast')
