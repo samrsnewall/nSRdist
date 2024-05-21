31fa3096221632cbf79fe5ca34b12a5252b1d08b
samnewall <samnewall@snewall-macmini.geol.ucsb.edu>
2024-05-21 11:26:41 -0700

    fixed code to get high resolution subset improved commenting

--- invSR_MSPF2_functions.m
+++ invSR_MSPF2_functions.m
@@ -1,59 +1,74 @@
 %% ReadMe
-%We use corechoices_MSPF.xlsx, which has metadata for each core, including
-%the labIDs of the samples that are MSPF of the same species, and including
-%information on which data we believe should be removed (from visual
-%inspection).
+%This is the main script for creating a distribution of normalised
+%sedimentation rate from radiocarbon dated ocean sediment cores. The
+%radiocarbon data is taken from the World Atlas of late Quaternary
+%Foraminifera Oxygen and Carbon Isotope Ratios (Mulitza et al., 2022) -
+%henceforth called WA2022. The data is filtered using the excel file
+%COPYcorechoices_MSPF.xlsx, which contains the information needed to choose
+%only the monospecific planktonic foraminifera - henceforth MSPF dates from
+%each core. This excel file also allows for some dates to be removed "by
+%hand" to speed up computational process.
+
+%A few of the important choices made are:
+% - If >3/4 of possible pathways between pair of radiocarbon dates is negative, it is classified as a reversal
+% - The \DeltaR is 0±200yr
+% - Cores are split by SR at 8cm/kyr
+% - An extra subset is made by picking 10 highSR cores with most data
 
 %% Add Functions Folder to Path
 addpath('Functions')
 
 %% Load Metadata of MSPF cores
 %Check which cores have MSPF (monospecific planktonic foram) dates
-data = readtable("../CoreSpreadsheets/COPYcorechoices_MSPF.xlsx"); %read all metadata
+data = readtable("COPYcorechoices_MSPF.xlsx"); %read all metadata
 dataMSPF = data(data.MSPF == 1,:);
 
-%% Index Good Cores
+%------ Index Good Cores
 %Create index for all cores
 numAllCores = length(dataMSPF.CoreName);
 allcores = 1:numAllCores;
 %Create index of cores that have many reversals (determined by manual
 %inspection)
-reversalDenseCores = ["GeoB1711-4", "H214", "SO75_3_26KL", "MD95-2042", "KNR159-5-36GGC"];
-problemCores = ["MD98-2181", "SU81-18", "MD02-2550", "MD88-770"]; % These all have too many reversals...
-problemCores = ["MD98-2181"]; %#ok<NBRAK2>
+reversalDenseCores = ["GeoB1711-4", "H214", "SO75_3_26KL", "KNR159-5-36GGC"];
+%problemCores = ["SU81-18", "MD02-2550", "MD88-770"]; % These all have too many reversals 
+problemCores = [];
 badLog = contains(string(dataMSPF.CoreName),[reversalDenseCores, problemCores]);
 goodLog = badLog == 0;
 allLog = true(length(goodLog), 1);
 badIndexes = allcores(badLog);
+
 %Hence create index to use only cores that passed manual inspection
 goodIndexes = allcores(~badLog);
 
-%% Choose cores to analyse
+%------- Create a subset of cores to look at if interested
 %Decide which subset of cores to look at
 subsetchooser = logical(zeros(numAllCores, 1)); %#ok<LOGL>
-subsetchooser(145:155) = 1; 
+subsetchooser(7) = 1; 
 subsetchooser(badLog) = 0;
+
+%------- Take desired data into arrays
 chosenCoresLog = goodLog;
 cores = table2array(dataMSPF(chosenCoresLog, "CoreName")); %take list of MSPF corenames
 lats = table2array(dataMSPF(chosenCoresLog, "LatitudeDec"));
 longs = table2array(dataMSPF(chosenCoresLog,"LongitudeDec"));
 depths = table2array(dataMSPF(chosenCoresLog, "WaterDepthM"));
-LabIDs = table2array(dataMSPF(chosenCoresLog, "LabIDs")); %take list of LabIDs relating to MSPF dates of each core
-incDepths = table2array(dataMSPF(chosenCoresLog, "IncludeDepths")); % take list of depths (useful if no labels)
-excLabIDs = table2array(dataMSPF(chosenCoresLog, "excludeLabIDs")); %take list of manually removed dates for each core
-excDepths = table2array(dataMSPF(chosenCoresLog, "excludeDepth")); %take list of manually removed dates for each core (useful if no labels)
+LabIDs = table2cell(dataMSPF(chosenCoresLog, "LabIDs")); %take list of LabIDs relating to MSPF dates of each core
+incDepths = table2cell(dataMSPF(chosenCoresLog, "IncludeDepths")); % take list of depths (useful if no labels)
+excLabIDs = table2cell(dataMSPF(chosenCoresLog, "excludeLabIDs")); %take list of manually removed dates for each core
+excDepths = table2cell(dataMSPF(chosenCoresLog, "excludeDepth")); %take list of manually removed dates for each core (useful if no labels)
 numCores = sum(chosenCoresLog);
 
-%% Plot radiocarbon data from a single core (for exploratory use)
-% iPlot = 1;
+%------ Plot radiocarbon data from a single core (for exploratory use)
+% for iPlot = find(chosenCoresLog)
 % corePlot(cores{iPlot}, LabIDs{iPlot}, incDepths{iPlot}, excLabIDs{iPlot}, excDepths{iPlot})
+% end
 
 %% invSR PDF Approach
-% This section runs through a quicker, less-computationally-expensive
-% estimator of SR to find any reversals (and thence create scenarios to
-% avoid them), to calculate the meanSR, return number of dates used. 
+% This section runs through a quick, less-computationally-expensive
+% estimator of SR to find any reversals, create scenarios, to calculate the
+% meanSR and the number of dates used overall
 
-% Initialise variables to hold this information
+%------ Initialise variables to hold this information
 core_invSRvals = cell(numCores,1);
 core_invSRprobs = cell(numCores,1);
 meanSR = nan(numCores,1);
@@ -66,13 +81,7 @@ corescenarios = cell(numCores,1);
 newlabels = cell(numCores,1);
 numreversals = nan(numCores,1);
 
-% If I want to check a specific core
-    
-    % i = 106;
-    % disp(cores{i})
-    % [core_invSRvals{i}, core_invSRprobs{i}, meanSR(i), MSI_byage(i), MSI_bydepth(i), sedimentlength(i), num14cpairs(i), corescenarios{i}, newlabels{i}, numreversals(i)] = oneCoreSRpdf(cores{i}, LabIDs{i}, incDepths{i}, excLabIDs{i}, excDepths{i}, 0);
-
-%% Apply invSR with loop
+%----- Apply invSR with loop
 %Calculate SR distribution for each core, as well as meanSR and other
 %useful information
 parfor i = 1:numCores
@@ -80,27 +89,26 @@ parfor i = 1:numCores
     [core_invSRvals{i}, core_invSRprobs{i}, meanSR(i), MSI_byage(i), MSI_bydepth(i), sedimentlength(i), num14cpairs(i), corescenarios{i}, newlabels{i}, numreversals(i)] = oneCoreSRpdf(cores{i}, LabIDs{i}, incDepths{i}, excLabIDs{i}, excDepths{i}, 0);
 end
 
-%% Find high SR and low SR cores (separated by 8cm/kyr following Lin et al., 2014)
+%------ Define Subsets of interest
+% high SR and low SR cores (separated by 8cm/kyr following Lin et al., 2014)
 lowSRCoresLog = meanSR<= 8;
 highSRCoresLog = meanSR >8;
 
-% find top 10 highest resolution cores (high resolution means low Mean
-% Sampling Interval).
-[~,highRes10CoresInd] = maxk(-MSI_byage, 10);
-highRes10CoresLog = MSI_byage <= MSI_byage(highRes10CoresInd(end));
-highRes10HighSRCoresLog = highRes10CoresLog & highSRCoresLog;
+% find 10 highSR cores with most data
+% MSI_byageTOOL = MSI_byage.*highSRCoresLog; MSI_byageTOOL(isnan(MSI_byage) | MSI_byageTOOL == 0) = 9e9;
+% [~,highSRhighResCoresInd] = maxk(-MSI_byageTOOL, 10);
+[~,highSRhighResCoresInd] = maxk(num14cpairs.*highSRCoresLog, 10);
+highSRhighResCoresLog = unfind(highSRhighResCoresInd, numel(cores));
 
-[~,highRes15CoresInd] = maxk(-MSI_byage, 15);
-highRes15CoresLog = MSI_byage <= MSI_byage(highRes15CoresInd(end));
-
-%% Combine the invSR pdfs from each core
+%------ Combine the invSR pdfs from each core
 [master_invSRvals, allcore_invSRprobs] = combinepdfs(core_invSRvals, core_invSRprobs, sedimentlength);
 
-%% Convert to Sed Rate
+%------ Convert to Sed Rate
 %Change inverseSR distribution to SR distribution
 [SRvals_interp, SRvalsprob_norm] = invSRtoSR(master_invSRvals, allcore_invSRprobs);
 
-%% Plot the SR distribution from PDF method and compare with BIGMACS values
+%------ Plot the SR distribution from PDF method and compare with BIGMACS values
+
 %Load normalised SR data from BIGMACS
 normSR_BIGMACS = load("../BIGMACSdata/Lin2014_sedrateratio_cm_wo_NaN.txt");
 %Load lognormal file
@@ -135,43 +143,44 @@ legend("MSPF2 PDF")
 %and it can be used to weight by age, and we can create distributions of
 %age difference between radiocarbon pairs to test for a resolution effect.
 
-%Initiate variables
+%----- Initiate variables
 nSRcounts = cell(numCores,1); % Holds all the nSR counts (which form histogram that makes nSR pdf)
 agediffs = cell(numCores, 1); % Holds all the age differences for each nSR measurement (resolution pdf)
 coreTM = nan(3,3,numCores);
 cores_transnums = nan(3,3,numCores); %Counts of each type of transition
 cores_CSE2x = nan(3,1,numCores); %Counts of the number of transitions from each starting state
 
-%Run through all chosen cores with random sampling approach
+%------ Run through all chosen cores with random sampling approach
 parfor i =  1:numCores
     disp(cores{i})
     [cores_transnums(:,:,i), cores_CSE2x(:,:,i), nSRcounts{i}, agediffs{i}] = oneCoreTM(cores{i}, corescenarios{i}, LabIDs{i}, incDepths{i}, excLabIDs{i}, excDepths{i});
 end
 
-%% Calculate all core TM
+%----- Calculate all core TM
 %Calculate the number of transitions across all the cores
 allcores_transnums = sum(cores_transnums, 3, 'omitmissing');
 allcores_CSE2x = sum(cores_CSE2x, 3, 'omitmissing');
 allcores_CSE2x_ratios = allcores_CSE2x'./(sum(allcores_CSE2x));
 
-%Construct the transition matrix
+%----- Construct the transition matrix
 allTM = [allcores_CSE2x_ratios;allcores_transnums./allcores_CSE2x];
 
-%% Plot the histogram of random sample counts
+%% Plot histograms of random sample counts
+%Plot histograms for High SR subset
 plotSRandResHistograms(nSRcounts, agediffs, num14cpairs, highSRCoresLog, 101, 'r', "High SR")
+
+%Plot histograms for Low SR Subset
 plotSRandResHistograms(nSRcounts, agediffs, num14cpairs, lowSRCoresLog, 102, 'b', "Low SR")
 
-numNotNan = length(MSI_byage) - sum(isnan(MSI_byage));
-if numNotNan < 10
+%Plot 
+if sum(highSRCoresLog) >10
 else
-[~,highRes10CoresInd] = maxk(-MSI_byage, 10);
-highRes10CoresLog = MSI_byage <= MSI_byage(highRes10CoresInd(end));
-highRes10HighSRCoresLog = highRes10CoresLog & highSRCoresLog;
-plotSRandResHistograms(nSRcounts, agediffs, num14cpairs, highRes10HighSRCoresLog, 103, 'k', "10 Highest Res")
+plotSRandResHistograms(nSRcounts, agediffs, num14cpairs, highSRhighResCoresLog, 103, 'k', "10 Highest Res")
 end
 
-%% Plot Map of data in MATLAB
-%Find index of all cores used in the analysis
+%% Plot output metadata and figures
+
+%----- Find index of all cores used in the analysis
 ind2 = NaN(numCores,1);
 for i = 1:numCores
     if isempty(core_invSRvals{i})
@@ -182,92 +191,5 @@ for i = 1:numCores
 end
 ind3 = find(ind2);
 
-%Choose whether to further narrow down which cores to plot/summarise
-subsetSpecificI = 0;
-specificSubset = highSRCoresLog;
-if subsetSpecificI == 1
-    ind4 = NaN(numCores,1);
-    for i = 1:numCores
-        if specificSubset(i) == 0
-            ind4(i) = 0;
-        else
-            ind4(i) =1;
-        end
-    end
-    ind5 = find(ind2.*ind4);
-end
-
-%Find corenames, lats, longs, depths of cores included
-core_inc = cores(ind3);
-lat_inc = lats(ind3);
-lon_inc = longs(ind3);
-dep_inc = depths(ind3);
-meanSR_inc = meanSR(ind3);
-MSI_byage_inc = MSI_byage(ind3);
-MSI_bydepth_inc = MSI_bydepth(ind3);
-
-%Make plot with locations denoted as red stars
-figure(23)
-worldmap('World')
-setm(gca, 'mapprojection', 'robinson')
-geoshow('landareas.shp', 'FaceColor','[0.7 0.7 0.7]', 'EdgeColor', '[0.7 0.7 0.7]')
-load coastlines
-plotm(coastlat, coastlon, 'Color', 'k')
-hold on
-plotm(lat_inc, lon_inc, 'r*')
-%% Plot Histograms
-
-%Plot histograms of Mean SR, Depths, Resolution by Age, Resolution by Depth
-figure(29)
-subplot(4,1,1)
-histogram(meanSR_inc, 0:2:90, 'FaceColor','k')
-xlabel('Mean SR (cm/kyr)')
-ylabel('Counts')
-xlim([0 90])
-title("Cores' Mean SR")
-
-subplot(4,1,2)
-histogram(dep_inc./1000, 0:0.25:6, 'FaceColor', 'k')
-xlim([0 6])
-xlabel("Depth (km)")
-ylabel("Counts")
-xticks(0:0.5:6)
-title("Core Depths")
-
-subplot(4,1,3)
-histogram(MSI_byage_inc, 20, 'FaceColor','k')
-xlabel('Sampling Frequency by age (kyr/date)')
-ylabel('Counts')
-title("Cores' Mean Sampling Interval By Age")
-
-subplot(4,1,4)
-histogram(MSI_bydepth_inc, 20, 'FaceColor','k')
-%xlim([0 150])
-xlabel('Sampling Frequency by depth (cm/date)')
-ylabel('Counts')
-%xticks(0:25:150)
-title("Cores' Mean Sampling Interval By Depth")
-
-%% Display some important information
-
-%Input values from BIGMACS transition matrix
-BIGMACSTM = [0.7215, 0.0940, 0.1846; 0.4328, 0.2687, 0.2985; 0.2670, 0.1041, 0.6290];
-
-%Find out how much sediment the cores used constitute
-lengthsed_core = nan(length(nSRcounts),1);
-for i = 1:length(nSRcounts)
-    if isempty(nSRcounts{i})
-    else
-    lengthsed_core(i) = sum(nSRcounts{i}(2,:));
-    end
-end
-
-%Display total number fo 14C pairs used and total length of sediment used
-disp("The total number of 14C pairs used is")
-disp(sum(num14cpairs, 'omitmissing'))
-disp("The total length of sediment used is")
-disp(sum(sedimentlength, 'omitmissing'))
-disp(sum(lengthsed_core, 'omitmissing'))
-
-results = struct("CoreNames", cores, "Latitude", num2cell(lats), "Longitude", num2cell(longs), "Depths", num2cell(depths), "nSRcounts", nSRcounts, "agediffs", agediffs, "MeanSR", num2cell(meanSR), "ResolutionByAge", num2cell(MSI_byage), "ResolutionByDepth", MSI_bydepth, "Number14Cpairs", num14cpairs, "SedimentLength", sedimentlength);
+outputMetadataAndSummaryFigures(ind3, cores, lats, longs, depths, meanSR, MSI_byage, MSI_bydepth, nSRcounts)
 
