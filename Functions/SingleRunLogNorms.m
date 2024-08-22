fadb4771ae7269cd94ea74149224d76c148ffd87
Sam Newall <77326394+samrsnewall@users.noreply.github.com>
2024-08-21 17:27:17 -0700

    uses the makeWeightedReplicates function

--- Functions/SingleRunLogNorms.m
+++ Functions/SingleRunLogNorms.m
@@ -1,12 +1,12 @@
-function[SR_MixLogNorm1Run] = SingleRunLogNorms(nSRcounts, coreLog, numruns, x)
+function[SR_MixLogNorm1Run] = SingleRunLogNorms(nSRcounts, coreLog, numruns, x, weightQ, weightRepDP, weightRepInflator, regularizationVal)
 
 %Initialise vector
 SR_MixLogNorm1Run = NaN(length(x), numruns);
 
 %Clean out any cores that returned empty nSR vectors
 nSRcountsChosen = nSRcounts(coreLog);
-cores2remove = cellfun(@isempty,nSRcountsChosen);
-nSRcountsClean = nSRcountsChosen(~cores2remove);
+cores2remove    = cellfun(@isempty,nSRcountsChosen);
+nSRcountsClean  = nSRcountsChosen(~cores2remove);
 
 % Take 1 run's worth of data from each core and calculate mix log normals.
 for i = 1:numruns
@@ -15,44 +15,44 @@ OneRunData = [];
     for j = 1:length(nSRcountsClean)
 
       %Open the core's cell
-      nSRcount_openfile = nSRcountsClean{j};
+      nSRcount_opencell = nSRcountsClean{j};
 
       %Find the NaN's that split runs
-      split_index = find(isnan(nSRcount_openfile(1,:)));
-      split_indexStart = split_index + 1;
-      split_indexEnd = split_index - 1;
+      split_index       = find(isnan(nSRcount_opencell(1,:)));
+      split_indexStart  = split_index + 1;
+      split_indexEnd    = split_index - 1;
 
       % use the NaN indeces to choose the data of that run for each core
       % and concatenate it into OneRunData
-      nSRcount_1core1run = nSRcount_openfile(1:2,split_indexStart(i):split_indexEnd(i+1));
-      OneRunData = cat(2, OneRunData, nSRcount_1core1run);
+      nSRcount_1core1run = nSRcount_opencell(1:2,split_indexStart(i):split_indexEnd(i+1));
+      OneRunData         = cat(2, OneRunData, nSRcount_1core1run);
     end
 
     %% MUST ADD THIS WEIGHTING COMPONENT!
-    X = OneRunData(1,:)';
-    Y = OneRunData(2,:);
-    Xclean = X(~isnan(X)); %Remove NaNs that separate cores and runs
-    Yclean = Y(~isnan(X)); %Remove NaNs that separate cores and runs
-    [X_u, ~, IC] = unique(Xclean);
-    Y_u = accumarray(IC,Yclean);
-    Y_uR = round(Y_u.*100); %%%%%%%%%% NEED TO DO A SENSITIVITY TEST TO THIS! IF I DON'T MULTIPY Y_u BY 100 THEN MUCH DATA HAS ITS WEIGHTING ROUNDED DOWN TO 0 WEIGHT... TRY
-    data = repelem(X_u,Y_uR);
-
-    % %Fit a mixture Log Normal to the data (not weighted)
-    % [SR_MixLogNorm1RunHOLDER, ~, ~] = fitMixLogNorm(OneRunData(1,:), 2);
-
-    %Fit a mixture Log Normal to the data (weighted)
-    [SR_MixLogNorm1RunHOLDERweighted, ~, ~] = fitMixLogNorm(data, 2);
-
-    %Plot histogram and mix log norm for each run
-    if i < 6
-        figure(i);
-        hold on
-        histogram(data, "Normalization", "pdf", "BinEdges",[0:0.1:100, 1000])
-        plot(SR_MixLogNorm1RunHOLDERweighted(:,1), SR_MixLogNorm1RunHOLDERweighted(:,2), '-k', 'LineWidth', 1)
-        xlim([0 10])
+    if weightQ == 1 %This indicates whether to weight by depth or not
+        inputData       = OneRunData(1,:);
+        weights         = OneRunData(2,:);
+        inputDataClean  = inputData(~isnan(inputData));
+        weightsClean    = weights(~isnan(weights));
+        data            = makeWeightedReplicates(inputDataClean, weightsClean, weightRepDP, weightRepInflator); %Weight by replicating data according to weighting
+    else
+        %Set up data as unweighted input data
+        data = OneRunData(1,:);
     end
+    %Fit a mix log normal to the data
+    [SR_MixLogNorm1RunHOLDER, ~, ~] = fitMixLogNorm(data, x, 2, regularizationVal);
+
+    % %Plot histogram and mix log norm for each run
+    % if i < 6
+    %     figure;
+    %     hold on
+    %     histogram(data, "Normalization", "pdf", "BinEdges",[0:0.1:100, 1000])
+    %     plot(x, SR_MixLogNorm1RunHOLDER(:,2), '-k', 'LineWidth', 1)
+    %     xlim([0 10])
+    %     ylim([0 1.5])
+    %     xlabel("nSR")
+    % end
 
 %Save mix log norm pdf values 
-  SR_MixLogNorm1Run(:, i) = SR_MixLogNorm1RunHOLDERweighted(:,2);
+  SR_MixLogNorm1Run(:, i) = SR_MixLogNorm1RunHOLDER(:,2);
 end
