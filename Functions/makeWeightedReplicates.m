05e14443885385ecd4339bd0dc475971a8a46abd
Sam Newall <77326394+samrsnewall@users.noreply.github.com>
2024-08-21 17:26:53 -0700

    This function makes weighted replicates of data for my fitting of pdfs to weighted datasets

---
+++ Functions/makeWeightedReplicates.m
@@ -0,0 +1,14 @@
+function[repData] = makeWeightedReplicates(data, weight, dataRoundingDP, weightingInflationMultiplier)
+%%% This function takes some data and some weighting and creates
+%%% the variable repData, which is a single variable that approximates the
+%%% weighting of the data by replicating each x by approximately it's
+%%% weighting. For example, if x=3 has a weighting y=4, then the value 3
+%%% will be repeated 4 times in repData.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ data           = round(data,dataRoundingDP);                   %The data are rounded so that there are less unique values to help count weightings
+[data_u,~, IC]  = unique(data);                                 %The unique values of the data are found, with their indices
+ weight_u       = accumarray(IC,weight);                        %The weighting of each unique value is combined
+ weight_uR      = round(weight_u.*weightingInflationMultiplier);%The weighting of each unique value is converted to an integer
+ repData        = repelem(data_u,weight_uR);                    %The unique values are replicated according to their integer weighting
+end
