8932d42ec862c8e72a424df94cea6cfe0c81df9d
Sam Newall <77326394+samrsnewall@users.noreply.github.com>
2024-11-12 17:05:26 -0800

    Changed the matCal age vector from every 1 year to every 3 years to reduce array size by 3x

--- Functions/multiMatcal.m
+++ Functions/multiMatcal.m
@@ -1,4 +1,4 @@
-function[ageprob] = multiMatcal(age, error, date_is)
+function[ageprob, calAge] = multiMatcal(age, error, date_is, S)
 %This function calibrates multiple radiocarbon ages and returns an array
 %ageprob that holds the probability densities for each ith age in its ith
 %column. The array has 55001 rows, each row corresponds to the year row-1 
@@ -11,15 +11,24 @@ function[ageprob] = multiMatcal(age, error, date_is)
 
 % Set up reservoir age information
 resAge = 0;
-resError = 200;
+resError = S.DeltaRError;
+
+%Set up age vector
+% calAge = 0:55000;
+calAge = 0+1:3:55000-1; %Create vector of every 3 years, to reduce computational time
 
 %Initialise ageprob array
 numAges = length(date_is);
-ageprob = zeros(55001, numAges);
+ageprob = zeros(length(calAge), numAges);
 
 %Go through loop to calibrate each age, choosing how many ages you
-    for i = 1:numAges
-        [~,~,holder,~] = matcal(age(date_is(i))*1000, error(date_is(i))*1000,  'Marine20', 'CalBP', 'resage', resAge, 'reserr', resError, 'plot', 0);
-        ageprob(:,i) = holder(:,2);
+for i = 1:numAges
+    [~,~,holder,~] = matcal(age(date_is(i))*1000, error(date_is(i))*1000,  'Marine20', 'CalBP', 'resage', resAge, 'reserr', resError, 'plot', 0);
+    for j = 1:55001/3
+        matCalProbs(j) = sum(holder(j*3-2 : j*3, 2));
     end
+    ageprob(:,i) = matCalProbs;
+end
+
+
 end
