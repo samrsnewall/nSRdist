74b689b6d087bb66d87abaef9745c99885a9ae2b
Sam Newall <77326394+samrsnewall@users.noreply.github.com>
2024-11-12 17:03:50 -0800

    Added a rounding to depth to avoid floating point errors when checking for doubly dated depths

--- Functions/getDataWA.m
+++ Functions/getDataWA.m
@@ -4,6 +4,7 @@ fnm = fullfile(WA_path, "Age/", corename + ".age");
 %Read in radiocarbon data from the core
 depth_m = ncread(fnm, "Depth"); %(meters)
 depth_cm = depth_m.*100; %convert to cm
+depth_cm = round(depth_cm, 2); %Round to the nearest 100th of a cm to avoid floating point number problems
 age = ncread(fnm, "Age dated"); %(14C kyrs BP)
 error = ncread (fnm, "Age +Error"); %(14C kyrs BP)
 label = ncread(fnm, "Label"); %(Lab ID)
