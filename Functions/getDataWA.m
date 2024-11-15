169c98bea5c83815546e2db0eb616f747e66f136
Sam Newall <77326394+samrsnewall@users.noreply.github.com>
2024-11-14 16:33:50 -0800

    cleaned up

--- Functions/getDataWA.m
+++ Functions/getDataWA.m
@@ -2,11 +2,14 @@ function[age, depth_cm, error, label] = getDataWA(corename)
 WA_path = "/Applications/PaleoDataView/WA_Foraminiferal_Isotopes_2022";
 fnm = fullfile(WA_path, "Age/", corename + ".age");
 %Read in radiocarbon data from the core
-depth_m = ncread(fnm, "Depth"); %(meters)
-depth_cm = depth_m.*100; %convert to cm
-depth_cm = round(depth_cm, 2); %Round to the nearest 100th of a cm to avoid floating point number problems
-age = ncread(fnm, "Age dated"); %(14C kyrs BP)
-error = ncread (fnm, "Age +Error"); %(14C kyrs BP)
-label = ncread(fnm, "Label"); %(Lab ID)
-label = string(label);
+depth_m     = ncread(fnm, "Depth"); %(meters)
+depth_cm    = depth_m.*100; %convert to cm
+depth_cm    = round(depth_cm, 2); %Round to the nearest 100th of a cm to avoid floating point number problems
+
+age         = ncread(fnm, "Age dated"); %(14C kyrs BP)
+
+error       = ncread (fnm, "Age +Error"); %(14C kyrs BP)
+
+label       = ncread(fnm, "Label"); %(Lab ID)
+label       = string(label);
 end
