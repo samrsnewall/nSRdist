829752ce22a4566d3fb6caddbc034738c34cfe4f
Sam Newall <77326394+samrsnewall@users.noreply.github.com>
2024-11-20 10:46:48 -0800

    HUGE IMPROVEMENT; added what I'd call a dictionary of all agepairs'  agediffs that have been calculated before, so instead of doing the taxing calculation using unique, it can check whether it has already done that calculation and pull the result from the "dictionary".

--- Functions/agediffcalc.m
+++ Functions/agediffcalc.m
@@ -1,4 +1,4 @@
-function[agediff_vals, agediff_probsums] = agediffcalc(ageprob, ageVector, numpairs, S)
+function[agediff_vals, agediff_probsums, IDpairs, agediffV] = agediffcalc(ageprob, ageVector, numpairs, labels, IDpairs, agediffV, S)
 %This function calculates the age difference probability distribution
 %functions for neighbouring pairs of radiocarbon dates. In order to reduce
 %computational time, all calibrated radiocarbon pdfs have values reduced to
@@ -23,14 +23,20 @@ ageprob_Nans(ind1) = NaN;
 
 for m = 1:(numpairs)
 
-    %Find vector of years for which the two radiocarbon dates are greater
+    labelPair = labels(m) + "," + labels(m+1);
+    pairCheckLog = ismember(IDpairs, labelPair);
+    if sum(pairCheckLog) ~=0
+        collector = agediffV{pairCheckLog};
+        agediff_vals{m} = collector(:,1);
+        agediff_probsums{m} = collector(:,2);
+    else
+        %Find vector of years for which the two radiocarbon dates are greater
     %than S.pdfMinVal (using ageprob_Nans)
-    if m ==1
-        notNans1 = find(~isnan(ageprob_Nans(:,m))); %Find indices where there are no Nans in ageprob_Nans
-        ages1 = ageVector(notNans1);
-        probs1 = ageprob(notNans1, m);
-    end
-    notNans2 = find(~isnan(ageprob_Nans(:,m+1)));  
+
+    notNans1 = find(~isnan(ageprob_Nans(:,m))); %Find indices where there are no Nans in ageprob_Nans
+    ages1 = ageVector(notNans1);
+    probs1 = ageprob(notNans1, m);
+    notNans2 = find(~isnan(ageprob_Nans(:,m+1)));
     ages2 = ageVector(notNans2);
     probs2 = ageprob(notNans2, m+1);
 
@@ -45,8 +51,12 @@ for m = 1:(numpairs)
     [agediff_vals{m},~,k2] = unique(agediffs);
     agediff_probsums{m} = accumarray(k2,pair_probs(:));
 
-    %Save the trouble of repeating construction of notNans1 next iteration
-    ages1 = ages2;
-    probs1 = probs2;
+    IDpairs = [IDpairs; labelPair]; %#ok<AGROW>
+    agediffV = [agediffV; {[agediff_vals{m}, agediff_probsums{m}]}]; %#ok<AGROW>
+
+    % %Save the trouble of repeating construction of notNans1 next iteration
+    % ages1 = ages2;
+    % probs1 = probs2;
+    end
 end
 end
