716148a8d191137083558b93dc0e9595b6831a46
Sam Newall <77326394+samrsnewall@users.noreply.github.com>
2024-12-12 11:55:33 -0800

    now can handle when the last age of the calAge (previously threw errors in the randsample set up). Note, this assumes all ages are < 55000 cal year

--- Functions/oneCoreTMRestrict.m
+++ Functions/oneCoreTMRestrict.m
@@ -58,23 +58,36 @@ end
 % Randomly choose 1000 scenarios
 numruns = 1000;
 rndScen = randsample(1:length(scenarios), numruns, 'true');
-for ix = 1:numruns;
+
+%Create numruns many possible sedimentation histories, by random sampling
+for ix = 1:numruns
+    %Find which dates are in the random scenario
     date_bool = ismember(string(label), scenarios{rndScen(ix)});
     date_is = find(date_bool == 1);
 
-    %Choose ages in this scenario
+    %Get ages' probability vectors
     ageprob = ageprobAll(:,date_is);
 
     %get depths in this scenario
     depths_scenario = depth_cm(date_is);
 
+    %Assign single dates from the probability vectors either...
+    %%%% Using modes
     if S.useModes
         run_age = ageModes{rndScen(ix)};
         run_age = run_age';
     else
 
+        %%%% Using random sampling
+
+        %Initialise
         run_age = NaN(1,length(date_is));
-        for i = 1:length(date_is)
+
+        %Run through each date and randomly choose a year from it's
+        %probability vector, ensuring that each age is older than the age
+        %directly shallower than it
+        i = 1;
+        while i <= length(date_is)
             %Set up age probabilities so that every age is older than the
             %age of sample shallower than it
             if i == 1
@@ -91,6 +104,16 @@ for ix = 1:numruns;
                 ind1 = age_probs >= S.pdfMinVal;
                 poss_ages = poss_ages(ind1);
                 age_probs = age_probs(ind1);
+                if length(poss_ages) <= 1
+                    disp("Random sampling has reached the end of the ages vector (from Matcal) for core "  + corename + ": - next random sample will fail")
+                    disp("Taking precaution - forcing assumption that no radiocarbon ages are actually older than 55000 cal yr")
+                    %resetting this random sampling run
+                    i = 1;
+                    run_age = NaN(1,length(date_is));
+                    continue
+                end
+
+
                 run_age(i) = randsample(poss_ages, 1, true, age_probs);
                 %%% RESTRICTION CODE
                 %If next age is not more than x years greater than
@@ -100,6 +123,7 @@ for ix = 1:numruns;
                     run_age(i) = NaN;
                 end
             end
+            i = i+1;
         end
     end
 
@@ -108,7 +132,7 @@ for ix = 1:numruns;
     depthOfUsed = depths_scenario(datesUsed);
     runAgesOfUsed = run_age(datesUsed);
     %datesUsedStore(runN,:) = datesUsed;
-    
+
     % Calculate mean SR for that potential run
     meanSR_run = (depthOfUsed(end)-depthOfUsed(1))./(runAgesOfUsed(end)-runAgesOfUsed(1));
 
@@ -137,94 +161,4 @@ for ix = 1:numruns;
         agediffs = cat(2, agediffs,age_diffs);
     end
 end
-
-%
-% % Loop through scenarios to calculate TM
-% for i_sce = 1:length(scenarios)
-%     %Find the dates in the given scenario
-%     date_bool = ismember(string(label), scenarios{i_sce});
-%     date_is = find(date_bool == 1);
-%
-%     %Choose ages in this scenario
-%     ageprob = ageprobAll(:,date_is);
-%
-%     %get depths in this scenario
-%     depths_scenario = depth_cm(date_is);
-%
-%     %------ Run a number of random samples, sampling each age with positivity rule
-%     %Set number of random samples
-%     numruns = 1000;
-%     %initialize
-%     datesUsedStore = zeros(numruns, length(date_is));
-%     for runN = 1:numruns
-%         %------ Create a potential run of ages
-%         run_age = NaN(1,length(date_is));
-%         for i = 1:length(date_is)
-%             %Set up age probabilities so that every age is older than the
-%             %age of sample shallower than it
-%             if i == 1
-%                 ind1        = ageprob(:,i) >= S.pdfMinVal;
-%                 poss_ages   = calAge(ind1);
-%                 age_probs   = ageprob(ind1,i);
-%                 run_age(i)  = randsample(poss_ages, 1, true, age_probs);
-%             else
-%                 min_age = max(run_age(1:i-1));
-%                 possibleAgesLog = calAge>min_age;
-%                 poss_ages = calAge(possibleAgesLog);
-%                 age_probs = ageprob(possibleAgesLog,i);
-%                 age_probs = age_probs./sum(age_probs);
-%                 ind1 = age_probs >= S.pdfMinVal;
-%                 poss_ages = poss_ages(ind1);
-%                 age_probs = age_probs(ind1);
-%                 run_age(i) = randsample(poss_ages, 1, true, age_probs);
-%                 %%% RESTRICTION CODE
-%                 %If next age is not more than x years greater than
-%                 %previous accepted age, reject it and move to sample from next
-%                 %radiocarbon date (x stored in input minAgeDiff)
-%                 if run_age(i) - max(run_age(1:i-1)) < minAgeDiff
-%                     run_age(i) = NaN;
-%                 end
-%             end
-%         end
-%
-%         % Find out which ages were used and which weren't
-%         datesUsed = ~isnan(run_age);
-%         depthOfUsed = depths_scenario(datesUsed);
-%         runAgesOfUsed = run_age(datesUsed);
-%         datesUsedStore(runN,:) = datesUsed;
-%
-%         % Calculate mean SR for that potential run
-%         meanSR_run = (depthOfUsed(end)-depthOfUsed(1))./(runAgesOfUsed(end)-runAgesOfUsed(1));
-%
-%         %------ Calculate the sedrates for each pair of ages
-%         age_diffs = diff(runAgesOfUsed);
-%         dep_diffs = diff(depthOfUsed)';
-%         SRs = dep_diffs./age_diffs;
-%         if S.normWithRunMean
-%             normSRs = SRs./meanSR_run;
-%         else
-%             normSRs = SRs./(scenario_meanSR(i_sce)./1000);
-%         end
-%         %Add normSRs to vector to count them (with their weighting)
-%         weightingNormaliser = (sum(validScenariosBool).*numruns); %Find normalising value based on number of scenarios and number of runs
-%         nSRinfo = [normSRs; dep_diffs./weightingNormaliser; dep_diffs; age_diffs]; %Set up nSR info (nSR counts, weighting, depth differences, age differences)
-%
-%         %Store all nSR info in a single array, with NaNs separating info from
-%         %different runs (instead of NaN in age-diff row, the first age used
-%         %is stored, so that the whole time history can be created from
-%         %this)
-%         if runN == 1 && i_sce == 1
-%             nSRcounts = [[NaN; NaN; min(depthOfUsed); min(runAgesOfUsed)], nSRinfo];
-%             agediffs = age_diffs;
-%         else
-%             nSRcounts = cat(2, nSRcounts,[[NaN; NaN; min(depthOfUsed); min(runAgesOfUsed)], nSRinfo]);
-%             agediffs = cat(2, agediffs,age_diffs);
-%         end
-%
-%     end
-%
-%     % [uniqueDatesUsed, ia, ic] = unique(datesUsedStore, 'rows');
-%     % [uDUoccurences, uDUindex] = groupcounts(ic);
-%
-% end
 end
