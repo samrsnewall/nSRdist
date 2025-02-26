49ec3ff9e42dee8016ff4f1085097425d9bbce43
Sam Newall <77326394+samrsnewall@users.noreply.github.com>
2025-02-26 10:41:49 -0800

    can now iterate bin size to ensure bins contain Expected counts > 5, and takes into account any changes in bins done by chi2gof function before plotting

--- Functions/chi2gof_vsfunction.m
+++ Functions/chi2gof_vsfunction.m
@@ -1,4 +1,4 @@
-function[h,p,chiStat] = chi2gof_vsfunction(data, cdfFH, desiredSum, binN, fitS)
+function[h,p,chiStat] = chi2gof_vsfunction(data, cdfFH, numParams, desiredSum, binN, fitS)
 %% Chi2 GOF test of data against given distribution
 
 %Define some bins
@@ -8,32 +8,79 @@ binCenters  = (binEdges(1:end-1)+binEdges(2:end))./2;
 %Find how many observations are in each bin
 obsCounts     = histcounts(data, binEdges);
 numObsCounts  = sum(obsCounts);
-divisor       = numObsCounts./desiredSum;
+divisor       = numObsCounts/desiredSum;
 obsCountsDown = round(obsCounts./divisor);
 
 %Get the expected counts, given the hypothesised distribution
-expProbs = cdfFH(binEdges(2:end)) - cdfFH(binEdges(1:end-1));
-expCounts = expProbs.*sum(obsCountsDown);
+%Set up some preliminary bin edges
+interiorEdges = binEdges(2:end-1);
+expProbs = diff([0, cdfFH(interiorEdges), 1]);
+expCounts = expProbs.*desiredSum;
+
+if fitS.enforceBinSizeLimits
+    minCountNumber = 5;
+    maxCountNumber = 50;
+    [interiorEdges] = iterateBinSize(expCounts, cdfFH, interiorEdges, desiredSum, minCountNumber, maxCountNumber);
+    binEdges = sort([min(data), interiorEdges, max(data)]);
+    binCenters  = (binEdges(1:end-1)+binEdges(2:end))./2;
+
+    %Find how many observations are in each bin
+    obsCounts     = histcounts(data, binEdges);
+    numObsCounts  = sum(obsCounts);
+    divisor       = numObsCounts/desiredSum;
+    obsCountsDown = round(obsCounts./divisor);
+
+    expProbs = diff([0, cdfFH(interiorEdges), 1]);
+    expCounts = expProbs.*desiredSum;
+end
 
 %Perform chi2gof
-[h, p, chiStat] = chi2gof(binCenters, 'Frequency', obsCountsDown, 'Edges', binEdges,'Expected', expCounts, 'Emin', 1);
+[h, p, chiStat] = chi2gof(binCenters, 'Frequency', obsCountsDown, 'Edges', binEdges,'Expected', expCounts, 'Emin', 5, 'NParams', numParams);
+
+%If chistat has changed binedges, use those
+if numel(chiStat.edges) ~= numel(binEdges)
+    binEdges = chiStat.edges;
+    binCenters  = (binEdges(1:end-1)+binEdges(2:end))./2;
+
+    %Find how many observations are in each bin
+    obsCounts     = histcounts(data, binEdges);
+    numObsCounts  = sum(obsCounts);
+    divisor       = numObsCounts/desiredSum;
+    obsCountsDown = round(obsCounts./divisor);
+
+    %Get the expected counts, given the hypothesised distribution
+    %Set up some preliminary bin edges
+    interiorEdges = binEdges(2:end-1);
+    expProbs = diff([0, cdfFH(interiorEdges), 1]);
+    expCounts = expProbs.*desiredSum;
+
+end
 
 %Get pdf from cdf by differentiating
 step = 1e-5;
-pdfFH = @(t) (cdfFH(t+step)-cdfFH(t-step))/(2*step);
-x = linspace(min(data), max(data), 100);
+pdfFH = @(t) (cdfFH(t+step) - cdfFH(t-step))/(2*step);
 
-%Plot
-if fitS.dispChi2 == true;
+%Define x between the 0.01 and 99.99 percentile of distribution
+lowpct = fzero(@(t) cdfFH(t) - 0.0001, -5);
+highpct = fzero(@(t) cdfFH(t) - 0.9999, 5);
+x = linspace(lowpct, highpct, 100);
+
+%Plot figure, with function, observed histogram, expected histogram, pvalue
+%and chistat value
+if fitS.dispChi2 == true
+
+    %Display Result of chi2gof to commandline
     if h == 0
         disp("chi2gof: Accept H0; p = " +  num2str(p, 3) + "; chistat = " + num2str(chiStat.chi2stat, 3))
     else
         disp("chi2gof: Reject H0 - p = " + num2str(p, 3) + "; chistat = " + num2str(chiStat.chi2stat, 3))
     end
 
+    %Plot figure
     figure;
     hold on
 
+    %Plot histograms
     yyaxis("left")
     hObs = histogram('BinCounts',obsCountsDown, 'BinEdges', binEdges, 'FaceColor', 'r', "DisplayName", "Observed Counts");
     hExp = histogram('BinCounts', expCounts, 'BinEdges', binEdges, 'FaceColor', 'k', "DisplayName","Expected Counts");
@@ -41,14 +88,19 @@ if fitS.dispChi2 == true;
         ylabel("Counts")
     else
         ylabel("Downweighted Counts")
+    end
 
-    yyaxis("right")
-    lTested = plot(x, pdfFH(x), 'k', 'LineWidth', 2, "DisplayName", "Tested PDF");
-    ylabel("PDF")
+    if fitS.enforceBinSizeLimits == false
+        %Plot function
+        yyaxis("right")
+        lTested = plot(x, pdfFH(x), 'k', 'LineWidth', 2, "DisplayName", "Tested PDF"); %#ok<*NASGU>
+        ylabel("PDF")
+    end
 
+    %Add pval and chi2stat to legend
     lpval   = plot(nan, nan, 'LineStyle', 'none', 'DisplayName', "p = " + num2str(p, 3));
     lchi2stat = plot(nan, nan, 'LineStyle', 'none', 'DisplayName', "chi2stat = "  + num2str(chiStat.chi2stat, 3));
-    xlabel("log nSR")
+    %xlabel("log nSR") Note, it's not always log nSR...
     legend()
 end
 end
