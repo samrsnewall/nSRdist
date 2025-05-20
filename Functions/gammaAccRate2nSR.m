79aefd58945a1d817544714ce6fc2062ce92d418
Sam Newall <77326394+samrsnewall@users.noreply.github.com>
2025-05-20 16:31:38 -0700

    now uses accShape and accScale instead of assuming accMean

--- Functions/gammaAccRate2nSR.m
+++ Functions/gammaAccRate2nSR.m
@@ -1,4 +1,4 @@
-function[nSR, nSRprob] = gammaAccRate2nSR(accShape)
+function[nSR, nSRprob] = gammaAccRate2nSR(accShape, accScale)
 %This function takes in some value of alpha (shape factor) for a gamma
 %distribution, creates the resulting distribution and converts its units
 %from accumulation rate (yr/cm - as used in Bacon) to normalised sed rate
@@ -7,10 +7,13 @@ function[nSR, nSRprob] = gammaAccRate2nSR(accShape)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%Use shape factor to get scale factor
-accMeanYr = 200; %(yr/cm) Note, accMeanYr has no impact on nSR distribution, so value is somewhat arbitrary
-accMean = accMeanYr/1000; %(kyr/cm)
-accScale = accMean/accShape;
+%%Use shape factor to get scale factor
+% accMeanYr = 200; %(yr/cm) Note, accMeanYr has no impact on nSR distribution, so value is somewhat arbitrary
+% accMean = accMeanYr/1000; %(kyr/cm)
+% accScale = accMean/accShape;
+
+%%Use shape and scale factor to get accMean
+accMean = accScale*accShape;
 
 %Find y values for Gamma distribution on normalised SR
 x = 0:(accMean/500):accMean*60; %make x scale sufficiently large and high resolution based on accMean (This doesn't quite do its job - works for this given accMean)
