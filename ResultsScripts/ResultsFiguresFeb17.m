10eaea134f427c9ea2d153f6ef9d96ee021246da
Sam Newall <77326394+samrsnewall@users.noreply.github.com>
2025-08-08 09:26:49 -0700

    added some analysis

--- ResultsScripts/ResultsFiguresFeb17.m
+++ ResultsScripts/ResultsFiguresFeb17.m
@@ -12,11 +12,11 @@ BM.TM = readmatrix("../BIGMACSdata/transition_parameter.txt");
 
 %% Figure of All Data, Newall500 Sampling in nSR, log(nSR) and inv(nSR)
 % Load data
-dAD = load("../Results/dataT_PFandLin_R200M20_Feb4_fitFeb17.mat");
+dAD = load("../Results/dataT_PFandLin_R200M20_Feb4_fitFeb18.mat");
 d = dAD.d;
 dAllSampB = dAD.d.Bchron1RunFits.weightedC;
 dAllSampN = dAD.d.Newall1RunFits.weightedC;
-d2_label = "Newall500";
+dN500_label = "Newall500";
 
 %Create binedges in invnSR
 if max(cell2mat(dAllSampB)) < 15
@@ -38,7 +38,7 @@ numruns = 100;
 
 figure;
 subplot(3,1,1)
-title([d.label ;d2_label])
+title([d.label ;dN500_label])
 xlim([0 exp(2)])
 hold on
 ylabel("cm")
@@ -120,7 +120,7 @@ disp("Chosen data has " + num2str(length(dAD.d.dataT.cores)) + " cores, with " +
 %in my new dataset
 
 %% Chi-squared GOF test on 1 sampling of Newall500 sampling approach - with evenly spaced bars (except for end bars)
-rng(1)
+rng(4)
 r = randi(numruns, 1);
 
 f_log = @(x) log(x);
@@ -135,13 +135,170 @@ fitS.dispChi2 = true;
 
 [h1, p1, chiStat1, h2, p2, chiStat2] = chi2_dataVStwopdfVECs(log(dAD.d.Newall1RunFits.weightedC{r}), dAD.d.Newall1RunFits.numCpairs(r), 20,OneNewMLNpdf , OneNewInvGampdf, fitS);
 
-% Need to make histogram bars equally spaced
-
 %% Compare newall sampled data and fits to Bchron sampled data and fits
+
+datasetB1 = d.Bchron1RunFits.weightedC;
+dB1_label = "Bchron Sampling";
+
+datasetN500 = d.Newall1RunFits.weightedC;
+dN500_label = "Newall500";
+
+MLNB1_fx = NaN(length(d.x), numruns);
+MLNB1_pfx = NaN(length(d.x), numruns);
+for i = 1:100
+    [MLNB1_fx(:,i), MLNB1_pfx(:,i)] = px_to_pfx(d.x, d.Bchron1RunFits.MLNPDFs(:,i), @log);
+end
+
+InvGamsB1_fx = NaN(length(d.x), numruns);
+InvGamsB1_pfx = NaN(length(d.x), numruns);
+for i = 1:100
+    [InvGamsB1_fx(:,i), InvGamsB1_pfx(:,i)] = px_to_pfx(d.x, d.Bchron1RunFits.invGamPDFs(:,i), @log);
+end
+
+%Find which distribution fits better for each run
+chooseMLN_B1 = sum(d.Bchron1RunFits.MLNchiStats.p > d.Bchron1RunFits.InvGamChiStats.p);
+chooseInvGam_B1 = numruns - chooseMLN_B1;
+
+MLNR500_fx = NaN(length(d.x), numruns);
+MLNR500_pfx = NaN(length(d.x), numruns);
+for i = 1:numruns
+    [MLNR500_fx(:,i), MLNR500_pfx(:,i)] = px_to_pfx(d.x, d.Newall1RunFits.MLNPDFs(:,i), @log);
+end
+
+InvGamsR500_fx = NaN(length(d.x), numruns);
+InvGamsR500_pfx = NaN(length(d.x), numruns);
+for i = 1:numruns
+    [InvGamsR500_fx(:,i), InvGamsR500_pfx(:,i)] = px_to_pfx(d.x, d.Newall1RunFits.invGamPDFs(:,i), @log);
+end
+
+%Find which distribution fits better for each run
+chooseMLN_Newall500 = sum(d.Newall1RunFits.MLNchiStats.p > d.Newall1RunFits.InvGamChiStats.p);
+chooseInvGam_Newall500 = numruns - chooseMLN_Newall500;
+
+figure()
+subplot(2,1,1)
+xlim([-3 3])
+yyaxis left
+hold on
+ylabel("cm")
+for i = 1:numruns
+    hold on
+    if i ==1 
+    histogram(log(datasetB1{i}), 'BinEdges', logBinEdges, 'FaceAlpha', 0.05, 'FaceColor', 'k', 'EdgeColor','none', 'HandleVisibility', 'on', 'DisplayName', 'Histograms')
+    else
+    histogram(log(datasetB1{i}), 'BinEdges', logBinEdges, 'FaceAlpha', 0.05, 'FaceColor', 'k', 'EdgeColor','none', 'HandleVisibility', 'off')
+    end   
+end
+yyaxis right
+hold on
+plot(MLNB1_fx, MLNB1_pfx,'LineStyle', '-', 'Color', [0,0,1,0.1],'Marker','none' ,'HandleVisibility', 'off')
+plot(NaN, NaN,'LineStyle', '-', 'Color', [0,0,1], 'Marker','none', 'DisplayName', "MLNs: " + num2str(chooseMLN_B1))
+plot(InvGamsB1_fx, InvGamsB1_pfx,'LineStyle', '-', 'Color', [1,0,0,0.1],'Marker','none', 'HandleVisibility', 'off')
+plot(NaN, NaN,'LineStyle', '-', 'Color', [1,0,0], 'Marker','none', 'DisplayName', "InvGammas: " + num2str(chooseInvGam_B1))
+ylim([0 1])
+xlabel("nSR")
+ylabel("PDF")
+legend()
+title("Bchron Samplings")
+subplot(2,1,2)
+xlim([-3 3])
+yyaxis left
+hold on
+ylabel("cm")
+for i = 1:numruns
+    hold on
+    if i ==1 
+    histogram(log(datasetN500{i}), 'BinEdges', logBinEdges, 'FaceAlpha', 0.05, 'FaceColor', 'k', 'EdgeColor','none', 'HandleVisibility', 'on', 'DisplayName', 'Histograms')
+    else
+    histogram(log(datasetN500{i}), 'BinEdges', logBinEdges, 'FaceAlpha', 0.05, 'FaceColor', 'k', 'EdgeColor','none', 'HandleVisibility', 'off')
+    end   
+end
+yyaxis right
+hold on
+plot(MLNR500_fx, MLNR500_pfx,'LineStyle', '-', 'Color', [0,0,1,0.1],'Marker','none' ,'HandleVisibility', 'off')
+plot(NaN, NaN,'LineStyle', '-', 'Color', [0,0,1], 'Marker','none', 'DisplayName', "MLNs: " + num2str(chooseMLN_Newall500))
+plot(InvGamsR500_fx, InvGamsR500_pfx,'LineStyle', '-', 'Color', [1,0,0,0.1],'Marker','none', 'HandleVisibility', 'off')
+plot(NaN, NaN,'LineStyle', '-', 'Color', [1,0,0], 'Marker','none', 'DisplayName', "InvGammas: " + num2str(chooseInvGam_Newall500))
+ylim([0 1])
+xlabel("nSR")
+ylabel("PDF")
+title("Newall Samplings")
+legend()
 
 %% Check fits with changing restriction on Newall sampling approach
 
+datasetN0 = d.Newall1RunFits0.weightedC;
+dN0_label = "Newall0";
+
+MLNR0_fx = NaN(length(d.x), numruns);
+MLNR0_pfx = NaN(length(d.x), numruns);
+for i = 1:numruns
+    [MLNR0_fx(:,i), MLNR0_pfx(:,i)] = px_to_pfx(d.x, d.Newall1RunFits0.MLNPDFs(:,i), @log);
+end
+
+InvGamsR0_fx = NaN(length(d.x), numruns);
+InvGamsR0_pfx = NaN(length(d.x), numruns);
+for i = 1:numruns
+    [InvGamsR0_fx(:,i), InvGamsR0_pfx(:,i)] = px_to_pfx(d.x, d.Newall1RunFits0.invGamPDFs(:,i), @log);
+end
+
+%Find which distribution fits better for each run
+chooseMLN_Newall0 = sum(d.Newall1RunFits0.MLNchiStats.p > d.Newall1RunFits0.InvGamChiStats.p);
+chooseInvGam_Newall0 = numruns - chooseMLN_Newall0;
+
+
+figure()
+subplot(2,1,1)
+xlim([-3 3])
+yyaxis left
+hold on
+ylabel("cm")
+for i = 1:numruns
+    hold on
+    if i ==1 
+    histogram(log(datasetN0{i}), 'BinEdges', logBinEdges, 'FaceAlpha', 0.05, 'FaceColor', 'k', 'EdgeColor','none', 'HandleVisibility', 'on', 'DisplayName', 'Histograms')
+    else
+    histogram(log(datasetN0{i}), 'BinEdges', logBinEdges, 'FaceAlpha', 0.05, 'FaceColor', 'k', 'EdgeColor','none', 'HandleVisibility', 'off')
+    end   
+end
+yyaxis right
+hold on
+plot(MLNR0_fx, MLNR0_pfx,'LineStyle', '-', 'Color', [0,0,1,0.1],'Marker','none' ,'HandleVisibility', 'off')
+plot(NaN, NaN,'LineStyle', '-', 'Color', [0,0,1], 'Marker','none', 'DisplayName', "MLNs: " + num2str(chooseMLN_Newall0))
+plot(InvGamsR0_fx, InvGamsR0_pfx,'LineStyle', '-', 'Color', [1,0,0,0.1],'Marker','none', 'HandleVisibility', 'off')
+plot(NaN, NaN,'LineStyle', '-', 'Color', [1,0,0], 'Marker','none', 'DisplayName', "InvGammas: " + num2str(chooseInvGam_Newall0))
+ylim([0 1])
+xlabel("nSR")
+ylabel("PDF")
+legend()
+title("Newall0 Samplings")
+subplot(2,1,2)
+xlim([-3 3])
+yyaxis left
+hold on
+ylabel("cm")
+for i = 1:numruns
+    hold on
+    if i ==1 
+    histogram(log(datasetN500{i}), 'BinEdges', logBinEdges, 'FaceAlpha', 0.05, 'FaceColor', 'k', 'EdgeColor','none', 'HandleVisibility', 'on', 'DisplayName', 'Histograms')
+    else
+    histogram(log(datasetN500{i}), 'BinEdges', logBinEdges, 'FaceAlpha', 0.05, 'FaceColor', 'k', 'EdgeColor','none', 'HandleVisibility', 'off')
+    end   
+end
+yyaxis right
+hold on
+plot(MLNR500_fx, MLNR500_pfx,'LineStyle', '-', 'Color', [0,0,1,0.1],'Marker','none' ,'HandleVisibility', 'off')
+plot(NaN, NaN,'LineStyle', '-', 'Color', [0,0,1], 'Marker','none', 'DisplayName', "MLNs: " + num2str(chooseMLN_Newall500))
+plot(InvGamsR500_fx, InvGamsR500_pfx,'LineStyle', '-', 'Color', [1,0,0,0.1],'Marker','none', 'HandleVisibility', 'off')
+plot(NaN, NaN,'LineStyle', '-', 'Color', [1,0,0], 'Marker','none', 'DisplayName', "InvGammas: " + num2str(chooseInvGam_Newall500))
+ylim([0 1])
+xlabel("nSR")
+ylabel("PDF")
+title("Newall500 Samplings")
+legend()
+
 %% Compare histograms from depth-weighted, age-weighted, unweighted
+
 
 %% Check impact of using single mean SR vs actual mean SR for sampling
 
