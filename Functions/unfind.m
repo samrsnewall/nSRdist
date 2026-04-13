e933ff4aaf583eac9c7960f9ea29be207da5520b
Sam Newall <sam.newall@mac.com>
2026-04-13 16:10:47 -0700

    removed functions not used

--- Functions/unfind.m
+++
@@ -1,8 +0,0 @@
-function lgc = unfind(idx, N)
-%This function returns a logical from a set of indices. It is like the
-%reverse of the find function.
-
-    %Go from indicies into logical (for vectors only)
-    lgc = false(N,1);
-    lgc(idx) = true;
-end
